ed1d576475944e4fff21cb98f2a3e0e3e0ed2309
NiklasJang <niklasjang@gmail.com>
2018-12-06 19:28:10 +0900

    Genetic Algorithm

---
+++ GeneticAlgorithm.py
@@ -0,0 +1,35 @@
+import random
+import string
+
+POOL_SIZE = 10
+'''
+Genetic Algorithm
+
+
+'''
+class Child:
+	def __init__(self,_index):
+		self.index = _index
+		self.string= ""
+
+
+	def getStr(self):
+		for i in range(10):
+			rndChar = random.choice(string.ascii_lowercase)
+			self.string +=rndChar
+		return self.string
+
+#Get Input String from user.
+print("Please enter myStr>> ",end="")
+myStr = input()
+print("Entered str is %s\n" % myStr)
+
+#Create an Child class array of size POOL_SIZE
+children = []
+for i in range(POOL_SIZE):	
+	children.append(Child(i))
+child1 = Child(1)
+print("== Generated children ==")
+for i in range(len(children)):
+	print("index %d is %s" %(i, children[i].getStr()))
+print("========================",end="")	
